671c01ce99957a904d17f161901ed09165ebf2ed
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-02-17 01:02:12 -0700

    Create solution2.py

---
+++ 141. Linked List Cycle/solution2.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        previously_seen = set()
+        while head is not None:
+            if head in previously_seen:
+                return True
+            previously_seen.add(head)
+            head = head.next
+        return False
