b621317d37c2ac24ec829f7613766960a7eaecf8
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-02-18 18:07:52 -0700

    Update solution2.py

--- 20. Valid Parentheses/solution2.py
+++ 20. Valid Parentheses/solution2.py
@@ -1,10 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         s = list(s)
-        dict = {'(': ')', '[': ']', '{': '}'}
+        dic = {'(': ')', '[': ']', '{': '}'}
         stack = []
         while(len(s) > 0):
-            if (s[0] in dict.keys()): stack.append(dict.get(s[0]))
+            if (s[0] in dic.keys()): stack.append(dic.get(s[0]))
             elif (not stack): return False
             elif (s[0] != stack.pop()): return False
             s.pop(0)
