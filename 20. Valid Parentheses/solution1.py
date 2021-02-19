1ef730165ac597dc5da3ce0e2775339a76f618a8
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-02-18 18:07:06 -0700

    Update solution1.py

--- 20. Valid Parentheses/solution1.py
+++ 20. Valid Parentheses/solution1.py
@@ -3,10 +3,10 @@ from queue import LifoQueue
 class Solution:
     def isValid(self, s: str) -> bool:
         s = list(s)
-        dict = {'(': ')', '[': ']', '{': '}'}
+        dic = {'(': ')', '[': ']', '{': '}'}
         stack = LifoQueue(maxsize = 1e4)
         while(len(s) > 0):
-            if (s[0] in dict.keys()): stack.put(dict.get(s[0]))
+            if (s[0] in dic.keys()): stack.put(dic.get(s[0]))
             elif (stack.empty()): return False
             elif (s[0] != stack.get()): return False
             s.pop(0)
