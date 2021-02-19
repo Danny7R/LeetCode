3d8ed86913509c8fde9fa1859d83ba7e02773bec
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-02-18 18:11:06 -0700

    Update solution3.py

--- 20. Valid Parentheses/solution3.py
+++ 20. Valid Parentheses/solution3.py
@@ -1,11 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mapping = {")": "(", "}": "{", "]": "["}
+        dic2 = {")": "(", "}": "{", "]": "["}
         for char in s:
-            if char in mapping:
+            if char in dic2:
                 top_element = stack.pop() if stack else '#'
-                if mapping[char] != top_element: return False
+                if dic2[char] != top_element: return False
             else: stack.append(char)
 
         return not stack
