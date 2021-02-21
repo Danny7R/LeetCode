ea99c05d53b05c213e9382544d7b0c0669d249c1
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-02-21 00:19:34 -0700

    Update solution2.py

--- 200. Number of Islands/solution2.py
+++ 200. Number of Islands/solution2.py
@@ -1,8 +1,9 @@
-def numIslands(self, grid):
-    def dfs(i, j):
-        if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
-            grid[i][j] = '0'
-            map(dfs, (i+1, i-1, i, i), (j, j, j+1, j-1))
-            return 1
-        return 0
-    return sum(dfs(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
+class Solution:
+    def numIslands(self, grid):
+        def dfs(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+                grid[i][j] = '#'
+                list(map(dfs, (i+1, i-1, i, i), (j, j, j+1, j-1)))
+                return 1
+            return 0
+        return sum(dfs(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
