6373f6f128f0b2cb1414b180865863c6913c01c2
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-02-20 23:32:13 -0700

    Update solution1.py

--- 200. Number of Islands/solution1.py
+++ 200. Number of Islands/solution1.py
@@ -1,11 +1,20 @@
-import numpy as np
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        grid = np.array(grid).astype('bool')
-        c = 1 if 1 in grid[0] else 0
-        for i in range(1, len(grid)):
-            overlap = np.any(np.logical_and(grid[i], grid[i-1]))
-            print(overlap)
-            c += 1 if (~overlap and np.any(grid[i]) == 1) else 0
+        if not grid: return 0
+        
+        c = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    c += 1
         return c
+
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
