f04397ab444ed1154575cb057e1cf525381b4ece
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-03-03 14:08:06 -0700

    Update solution2_(DFS).py

--- 111. Minimum Depth of Binary Tree/solution2_(DFS).py
+++ 111. Minimum Depth of Binary Tree/solution2_(DFS).py
@@ -8,6 +8,6 @@ class Solution:
     def minDepth(self, root: TreeNode) -> int:
         if not root: return 0
         if None in [root.left, root.right]:
-            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+            return 1 + max(self.minDepth(root.left), self.minDepth(root.right))
         else:
-            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
