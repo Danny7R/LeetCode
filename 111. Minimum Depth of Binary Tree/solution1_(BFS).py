32d63f2921abe461af8678306c1afe570b0c5ffc
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-03-03 13:28:27 -0700

    Create solution1_(BFS).py

---
+++ 111. Minimum Depth of Binary Tree/solution1_(BFS).py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        level = [root]
+        depth = 0
+        while level:
+            depth += 1
+            nextlevel = []
+            for node in level:
+                if not node.left and not node.right: return depth
+                if node.left: nextlevel.append(node.left)
+                if node.right: nextlevel.append(node.right)
+            level = nextlevel
+        # return depth
