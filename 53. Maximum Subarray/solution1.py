cbd7c38dd2f33835ea90e93d895afaf319476f84
Mohammadhossein Reshadi <mh.reshadi@gmail.com>
2021-03-03 21:58:37 -0700

    Update solution1.py

--- 53. Maximum Subarray/solution1.py
+++ 53. Maximum Subarray/solution1.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if not nums: return 0
         max_final = max_i = nums[0]
         for num in nums[1:]:
             max_i = max(num, max_i + num)
